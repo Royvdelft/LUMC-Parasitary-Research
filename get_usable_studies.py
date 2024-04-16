f7baffeac56e3fb9cda4ee93f4c48f6405eef9fc
David <capitalyseapp@gmail.com>
2024-04-16 13:31:47 +0200

    mooie dingen

--- get_usable_studies.py
+++ get_usable_studies.py
@@ -17,24 +17,64 @@ from io import StringIO
 # Initialize dictionaries to store file type counts and file size aggregates per study
 study_file_type_counts = {}
 study_file_size_aggregates = {}
+token = None
 
 def main():
 
-    # Get usable malaria studies
-    response = fetch_studies("malaria")
+    # First we get the accesstoken
+    global token
+    total_studies_amt = 0
+    passes_checks_amt = 0
+    studies_that_pass = []
+    username = "davidfraterman"
+    password = "D228cw75@"
 
+    token = generate_access_token(username, password)
+
+    # Then fetch all the studies that match the search term
+    response = fetch_studies_by_searchterm("malaria")
+
+    # And for each study that matches, aggregate the file data
     for study in response:
+        total_studies_amt += 1
         study_id = study['_id']
-        response = get_study(study_id)
+        response = get_study_files(study_id)
 
         aggregate_study_data(study_id, response)
 
 
-    studies = pick_usable_studies()
-    print('usable studies', studies)
+    # Pick the studies that pass the aggregation check
+    studies = filter_studies_with_files()
+
+    # For each study that passes the check, get the study info
+    for study_id in studies:
+        study_data = get_study_info(study_id)
+        passes_template_check = template_check(study_data)
+
+        if(passes_template_check):
+            passes_checks_amt += 1
+            studies_that_pass.append(study_id)
+
+    print(f"Studies that pass the template check: {studies_that_pass}")
+    print(f" --- ")
+    print(f"Total studies: {total_studies_amt}")
+    print(f"Studies that pass the template check: {passes_checks_amt}")
+    percentage = (passes_checks_amt / total_studies_amt) * 100
+    print(f"Percentage of studies that pass the template check: {percentage}%")
 
 
-def fetch_studies(search_term):
+
+def generate_access_token(username, password):
+    # Send a POST request to obtain the access token
+    response = requests.post("https://www.immport.org/auth/token", data={"username": username, "password": password})
+
+    # Extract the access token from the response
+    token = response.json()["access_token"]
+
+    return token
+
+
+def fetch_studies_by_searchterm(search_term):
     req = requests.get(f'https://www.immport.org/shared/data/query/api/search/study?term={search_term}&pageSize=100&fromRecord=0&sortField=&sortFieldDirection=', headers={
         'Content-Type': "application/json"
     })
@@ -47,23 +87,18 @@ def fetch_studies(search_term):
     return json_data['hits']['hits']
 
 
-def get_study(study_id):
-    print(study_id)
-
-    # Send a POST request to obtain the access token
-    response = requests.post("https://www.immport.org/auth/token", data={"username": "davidfraterman", "password": "D228cw75@"})
-
-    # Extract the access token from the response
-    token = response.json()["access_token"]
+def get_study_files(study_id):
+    global token
 
     # Use the access token in a subsequent request
     headers = {"Authorization": f"Bearer {token}"}
     response = requests.get(f"https://www.immport.org/data/query/result/filePath?format=csv&studyAccession={study_id}", headers=headers)
 
+
     if response.status_code == 200:
         # Parse response as JSON
         json_data = json.loads(response.content)
-
+        # print(json_data)
         return json_data
     else:
         sys.exit(1)
@@ -92,7 +127,7 @@ def aggregate_study_data(study_id, file_data):
     print(study_id + " processed successfully")
 
 
-def pick_usable_studies():
+def filter_studies_with_files():
      # For each object in study file type counts, check if has any key with value of more than 0
     studies = []
     global study_file_type_counts
@@ -102,6 +137,32 @@ def pick_usable_studies():
             studies.append(study_id)
 
     return studies
+
+
+def get_study_info(study_id):
+    headers = {"Authorization": f"Bearer {token}"}
+    response = requests.get(f"https://www.immport.org/data/query/api/study/{study_id}", headers=headers)
+    # print(study)
+
+    if response.status_code == 200:
+        # Parse response as JSON
+        study_data = response.json()[0]
+        return study_data
+    else:
+        sys.exit(1)
+
+
+def template_check(study_data):
+
+    # if has a brief description, actual start date, actual completion date, and actual enrollment and the values are not None
+    if (study_data['briefDescription'] is not None 
+        and study_data['actualStartDate'] is not None 
+        and study_data['actualCompletionDate'] is not None 
+        and study_data['actualEnrollment'] is not None
+        and study_data['endpoints'] is not None):
+        return True
+
+    return False
 
 
 if __name__ == "__main__":
